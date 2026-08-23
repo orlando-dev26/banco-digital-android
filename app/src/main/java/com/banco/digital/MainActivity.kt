31f6c5779625ef1a9d085cae352574c4ae97e923
orlando-dev26 <orlando-dev26@users.noreply.github.com>
2026-08-23 04:12:48 -0500

    feat: implementacion inicial de HomeScreen y CardsScreen responsive

---
+++ app/src/main/java/com/banco/digital/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.banco.digital
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.banco.digital.ui.screens.HomeScreen
+import com.banco.digital.ui.theme.DigitalBankAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DigitalBankAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
